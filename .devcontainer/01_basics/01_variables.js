1d7965efdb50db80846f8be7a6cc3216a138d597
Pranjal4523 <vyawaharepranjal@gmail.com>
2024-12-06 10:28:48 +0000

    datatpes

--- .devcontainer/01_basics/01_variables.js
+++
@@ -1,7 +0,0 @@
-const accountId=12221
-let accountEmail="pranjal@goole.com"
-var accountPassword = "12345"
-accountCity="pune"
-
-//accountId=11111
-console.log(accountId);
---
+++ basics/dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as never version
+
+//alert(3+3)  // we are using nodejs, not browser
+
+console.log(3
+    +3)     // its ok but code reusability should be high
+
+console.log("pranjal")  
+
+let name="pranju"       // string
+let age= 22;            //number
+let isLoggedIn = true;  // boolean 
+
+let state=null;  // empty
+
+//number ==>2 to power 53
+//bigint
+// string =->'' or""
+//boolean ==> true/false
+//null ==> standlone value  (empty value)
+// undefined ==> value is not assing yet
+//symbole ==>unique
+
+
+
+//object  (type of null is object)
+
+console.log(typeof age);    //number
+console.log(typeof state);  //object
+console.log(typeof undefined );  //undefined
